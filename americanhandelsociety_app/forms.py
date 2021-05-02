15de4c8e1400b81d95d17b95a651987b61027fa6
Regina Compton <reginafcompton@gmail.com>
2021-05-01 22:08:06 -0400

    fix: Make all Address form field optional

--- americanhandelsociety_app/forms.py
+++ americanhandelsociety_app/forms.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django import forms
 from django.forms import ModelForm
 
 from .models import Member, Address
@@ -11,6 +12,13 @@ class MemberChangeForm(UserChangeForm):
 
 
 class AddressChangeForm(ModelForm):
+    street_address = forms.CharField(required=False)
+    street_address_2 = forms.CharField(required=False)
+    city = forms.CharField(required=False)
+    state_province_region = forms.CharField(required=False)
+    zip_postal_code = forms.CharField(required=False)
+    country = forms.CharField(required=False)
+
     class Meta:
         model = Address
         fields = "__all__"
