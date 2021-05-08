462c4db9a8d91a707cfa0507c42d9f041e40621c
Regina Compton <reginafcompton@gmail.com>
2021-05-07 21:27:11 -0400

    task: Add function that gets called upon receiving IPN signal

--- americanhandelsociety_app/apps.py
+++ americanhandelsociety_app/apps.py
@@ -3,3 +3,9 @@ from django.apps import AppConfig
 
 class AmericanHandelSocietyAppConfig(AppConfig):
     name = "americanhandelsociety_app"
+
+    def ready(self):
+        from paypal.standard.ipn.signals import valid_ipn_received
+        from .signals import listen_for_paypal_please
+
+        valid_ipn_received.connect(listen_for_paypal_please)
