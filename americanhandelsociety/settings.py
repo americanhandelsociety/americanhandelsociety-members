3162eeff1e5e6b95af959dc47a28aae24225bdc5
Regina Compton <reginafcompton@gmail.com>
2021-12-20 21:17:30 -0500

    Test

--- americanhandelsociety/settings.py
+++ americanhandelsociety/settings.py
@@ -145,10 +145,16 @@ DATETIME_FORMAT = "%B %-d, %Y, %-I:%M"
 # STATIC_URL = "static/"
 # STATIC_ROOT = BASE_DIR / "americanhandelsociety_app/static"
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_URL = '/static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATICFILES_DIRS = ['static/newsletters']
+# STATICFILES_DIRS = ['americanhandelsociety_app/static']
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.1/howto/static-files/
+STATIC_URL = "/static/"
+STATIC_ROOT = "/static"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
