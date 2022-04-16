4b085fdba5f2cc168bdc92585a96ccce549e1636
Emily Edmondson <ecedmondson@gmail.com>
2022-04-16 18:54:45 -0500

    [Issue-104] add an updated_past_month value for list_display in admin

--- americanhandelsociety_app/admin.py
+++ americanhandelsociety_app/admin.py
@@ -1,3 +1,6 @@
+from datetime import datetime, timezone
+from dateutil.relativedelta import relativedelta
+
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
@@ -6,7 +9,25 @@ from .models import Member, Address
 
 class Admin(UserAdmin):
     model = Member
-    list_display = ("first_name", "last_name", "email", "id", "address")
+    list_display = (
+        "first_name",
+        "last_name",
+        "email",
+        "id",
+        "address",
+        "updated_past_month",
+    )
+
+    def updated_past_month(self, obj):
+        if not obj.last_updated:
+            return None
+        updated_past_month = obj.last_updated >= (
+            datetime.utcnow().replace(tzinfo=timezone.utc) + relativedelta(days=-30)
+        )
+        return updated_past_month or None
+
+    updated_past_month.short_description = "Updated Past Month?"
+
     list_filter = (
         "email",
         "available_in_directory",
