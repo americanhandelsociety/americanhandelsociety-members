0772c09a511c0d75422001f0dbf8c2c3b2ac6ae1
Emily Edmondson <ecedmondson@gmail.com>
2022-04-16 18:56:35 -0500

    [Issue-104] add filter for updated past month
    a few notes, which I will put into the PR description as well:
    This all could have been easier if you could filter based on model
    properties or functions that are not fields. You cannot. Even if you try.
    
    And I tried.
    
    You cannot do this even if you try to put a function similar to americanhandelsociety_app.admin.Admin's
    updated_past_month on americanhandelsociety_app.models.Member instead and then have your custom filter
    and your Admin display field function call that property/method. What happens is that the Admin site
    will 302 and redirect to <url>/<model>/?e=1, whatever that means, and not display the filter properly.
    
    As far as I can tell, the reason for this is that the filter QUITE LITERALLY expects a filtered queryset.
    NOT an exclude. Not a UNION. nothing else.
    
    So here we are.

--- americanhandelsociety_app/admin.py
+++ americanhandelsociety_app/admin.py
@@ -6,6 +6,27 @@ from django.contrib.auth.admin import UserAdmin
 
 from .models import Member, Address
 
+from django.contrib.admin import SimpleListFilter
+
+
+class UpdatedPastMonthFilter(SimpleListFilter):
+    title = "Updated Past Month"
+    parameter_name = "updated_past_month"
+
+    def lookups(self, request, model_admin):
+        return [
+            ("updated", "Updated Past Month"),
+        ]
+
+    def queryset(self, request, queryset):
+        if not self.value():
+            return queryset
+        past_month = datetime.utcnow().replace(tzinfo=timezone.utc) + relativedelta(
+            days=-30
+        )
+        if self.value() == "updated":
+            return queryset.filter(last_updated__gte=past_month)
+
 
 class Admin(UserAdmin):
     model = Member
@@ -35,6 +56,7 @@ class Admin(UserAdmin):
         "is_staff",
         "is_active",
         "id",
+        UpdatedPastMonthFilter,
     )
     fieldsets = (
         ("Authentication", {"fields": ("email", "password")}),
