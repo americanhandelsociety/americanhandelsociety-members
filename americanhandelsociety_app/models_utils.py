6485efdf0fd3730dfdf55a20395a8dc1ea885bea
Regina Compton <reginafcompton@gmail.com>
2024-01-05 19:04:55 -0500

    Refactor max_length

--- americanhandelsociety_app/models_utils.py
+++ americanhandelsociety_app/models_utils.py
@@ -4,4 +4,4 @@ from django.db import models
 class BaseTextChoices(models.TextChoices):
     @classmethod
     def max_length(cls):
-        return max(*(len(choice.value) for choice in cls))
+        return max((len(choice.value) for choice in cls))
