bfeb63155cc9b32543b8aff724fa83c535ee906e
Regina Compton <reginafcompton@gmail.com>
2024-12-13 14:49:45 -0500

    Change Zeffy URL

--- americanhandelsociety_app/constants.py
+++ americanhandelsociety_app/constants.py
@@ -1,5 +1,5 @@
 ZEFFY_EMBED_URL_FOR_RENEWAL_FORM = (
-    "https://www.zeffy.com/embed/ticketing/daa5a0a7-a8ff-4214-88eb-822fd0a78f8a"
+    "https://www.zeffy.com/embed/ticketing/membership-form-5"
 )
 
 RESEARCH_MATERIALS = {
--- tests/templatetags/test_profile_extras.py
+++ tests/templatetags/test_profile_extras.py
@@ -1,11 +1,12 @@
 import pytest
 
 from americanhandelsociety_app.templatetags.profile_extras import generate_renewal_url
+from americanhandelsociety_app.constants import ZEFFY_EMBED_URL_FOR_RENEWAL_FORM
 
 
 @pytest.mark.django_db
 def test_generate_renewal_url(member):
     assert (
         generate_renewal_url(member)
-        == "https://www.zeffy.com/embed/ticketing/daa5a0a7-a8ff-4214-88eb-822fd0a78f8a?modal=true&email=rodelinda%40lombardy.sa&firstName=Queen&lastName=Rodelinda"
+        == f"{ZEFFY_EMBED_URL_FOR_RENEWAL_FORM}?modal=true&email=rodelinda%40lombardy.sa&firstName=Queen&lastName=Rodelinda"
     )
